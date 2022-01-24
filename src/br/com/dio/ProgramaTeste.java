3f9b112d093d9789770f96c195f6a856d0db0671
mariana-98 <doccmari@gmail.com>
2022-01-24 15:53:53 -0800

    Initial commit

---
+++ src/br/com/dio/ProgramaTeste.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.Gato;
+
+public class ProgramaTeste {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        int a = 5;
+        int b = 4;
+
+        System.out.println("Hello, World! " + (a+b));
+    }
+}
+
---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,38 @@
+package br.com.dio;
+
+import java.util.Objects;
+
+public class Gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+    public Gato() {
+    }
+
+    public Gato(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+}
